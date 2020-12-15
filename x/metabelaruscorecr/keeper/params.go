6ee009a5c186ba412ed47bcff9e56c560ec046c5
Mark Vandal <arhat.pareen@gmail.com>
2020-12-15 18:06:11 +0100

    Added Cosmos boilerplate

---
+++ x/metabelaruscorecr/keeper/params.go
@@ -0,0 +1,22 @@
+package keeper
+/*
+// TODO: Define if your module needs Parameters, if not this can be deleted
+
+import (
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/markvandal/metabelaruscorecr/x/metabelaruscorecr/types"
+)
+
+// GetParams returns the total set of metabelaruscorecr parameters.
+func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
+	k.paramspace.GetParamSet(ctx, &params)
+	return params
+}
+
+// SetParams sets the metabelaruscorecr parameters to the param space.
+func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
+	k.paramspace.SetParamSet(ctx, &params)
+}
+*/
